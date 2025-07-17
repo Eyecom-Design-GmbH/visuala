16738373e1bd7b4a8b0fcc17027e395265101d36
EyecomDesign <71079977+EyecomDesign@users.noreply.github.com>
2025-07-17 13:44:30 +0200

    Removed some Funcs

--- src/angebot.js
+++ src/angebot.js
@@ -278,56 +278,56 @@ $(document).ready(function () {
     gsap.set(answer, { height: 0 });
   });
 
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".section-features",
-      start: "top 5%",
-      end: "bottom bottom",
-      scrub: true,
-      toggleActions: "restart none reverse",
-      pin: ".features-wrapper",
-    },
-  });
+  // let tl = gsap.timeline({
+  //   scrollTrigger: {
+  //     trigger: ".section-features",
+  //     start: "top 5%",
+  //     end: "bottom bottom",
+  //     scrub: true,
+  //     toggleActions: "restart none reverse",
+  //     pin: ".features-wrapper",
+  //   },
+  // });
 
-  gsap.set(".features-card", {
-    opacity: 1,
-    yPercent: 0,
-    xPercent: 0,
-    scale: 1,
-    rotation: (index) => {
-      if (index === 0) return -8;
-      if (index === 1) return 5;
-      return 0;
-    },
-  });
+  // gsap.set(".features-card", {
+  //   opacity: 1,
+  //   yPercent: 0,
+  //   xPercent: 0,
+  //   scale: 1,
+  //   rotation: (index) => {
+  //     if (index === 0) return -8;
+  //     if (index === 1) return 5;
+  //     return 0;
+  //   },
+  // });
 
-  tl.to(".features-card", {
-    duration: 0.3,
-  })
+  // tl.to(".features-card", {
+  //   duration: 0.3,
+  // })
 
-    .to(".features-card", {
-      yPercent: -130,
-      xPercent: (index) => (index % 2 === 0 ? 35 : -35),
-      scale: 1.25,
-      rotation: (index) => (index % 2 === 0 ? 15 : -15),
-      duration: 1.8,
-      ease: "power2.out",
-      stagger: { each: 1.8, from: "start" },
-    })
-    .to(
-      ".features-card",
-      {
-        opacity: 0,
-        yPercent: -150,
-        xPercent: (index) => (index % 2 === 0 ? 50 : -50),
-        rotation: (index) => (index % 2 === 0 ? 25 : -25),
-        scale: 0.8,
-        duration: 0.8,
-        ease: "power1.in",
-        stagger: { each: 1.8, from: "start" },
-      },
-      "-=0.5",
-    );
+  //   .to(".features-card", {
+  //     yPercent: -130,
+  //     xPercent: (index) => (index % 2 === 0 ? 35 : -35),
+  //     scale: 1.25,
+  //     rotation: (index) => (index % 2 === 0 ? 15 : -15),
+  //     duration: 1.8,
+  //     ease: "power2.out",
+  //     stagger: { each: 1.8, from: "start" },
+  //   })
+  //   .to(
+  //     ".features-card",
+  //     {
+  //       opacity: 0,
+  //       yPercent: -150,
+  //       xPercent: (index) => (index % 2 === 0 ? 50 : -50),
+  //       rotation: (index) => (index % 2 === 0 ? 25 : -25),
+  //       scale: 0.8,
+  //       duration: 0.8,
+  //       ease: "power1.in",
+  //       stagger: { each: 1.8, from: "start" },
+  //     },
+  //     "-=0.5",
+  //   );
 
   gsap.utils.toArray(".angebot_img").forEach((img, index) => {
     gsap.fromTo(
