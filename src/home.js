645074d0e4b9b593584ab4e0a606a3ad5cfa23c4
EyecomDesign <71079977+EyecomDesign@users.noreply.github.com>
2025-07-21 15:15:00 +0200

    Refactor project section animation configuration and implementation

--- src/home.js
+++ src/home.js
@@ -69,63 +69,63 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // Konfiguration für die Projekte-Sektion
-  const projekteConfig = {
-    id: "#project-section",
-    leftTitle: {
-      triggerStart: "top bottom",
-      triggerEnd: "top 50%",
-      blur: 5,
-      duration: 0.7,
-    },
-    rightTitle: {
-      triggerStart: "top 50%",
-      triggerEnd: "top top",
-      blur: 20,
-      duration: 1.2,
-    },
-    // Spezielle Selektoren für die Projekte-Sektion
-    leftSelector: ".angebot_display-heading-left-2",
-    rightSelector: ".angebot_display-heading-right-2",
-  };
+const projekteConfig = {
+  id: "#project-section",
+  leftTitle: {
+    triggerStart: "top bottom",
+    triggerEnd: "top 50%",
+    blur: 5,
+    duration: 0.7,
+  },
+  rightTitle: {
+    triggerStart: "top 50%",
+    triggerEnd: "top top",
+    blur: 20,
+    duration: 1.2,
+  },
+  // Standard-Selektoren (ohne -2 Suffix)
+  leftSelector: ".angebot_display-heading-left",
+  rightSelector: ".angebot_display-heading-right",
+};
 
-  // Animation für die Projekte-Sektion anwenden
-  const projekteSection = document.querySelector(projekteConfig.id);
+// Animation für die Projekte-Sektion anwenden
+const projekteSection = document.querySelector(projekteConfig.id);
 
-  if (projekteSection) {
-    // Linker Titel "UNSERE"
-    const leftHeading = projekteSection.querySelector(projekteConfig.leftSelector);
-    if (leftHeading) {
-      createTitleAnimation(leftHeading, {
-        direction: "left",
-        trigger: "scrub",
-        triggerElement: projekteSection,
-        triggerStart: projekteConfig.leftTitle.triggerStart,
-        triggerEnd: projekteConfig.leftTitle.triggerEnd,
-        distance: "100vw",
-        scale: 2.5,
-        opacity: 0,
-        blur: projekteConfig.leftTitle.blur,
-        duration: projekteConfig.leftTitle.duration,
-        ease: "power2.out",
-      });
-    }
-
-    // Rechter Titel "PROJEKTE"
-    const rightHeading = projekteSection.querySelector(projekteConfig.rightSelector);
-    if (rightHeading) {
-      createTitleAnimation(rightHeading, {
-        direction: "right",
-        trigger: "scrub",
-        triggerElement: projekteSection,
-        triggerStart: projekteConfig.rightTitle.triggerStart,
-        triggerEnd: projekteConfig.rightTitle.triggerEnd,
-        distance: "100vw",
-        scale: 2.5,
-        opacity: 0,
-        blur: projekteConfig.rightTitle.blur,
-        duration: projekteConfig.rightTitle.duration,
-        ease: "power2.out",
-      });
-    }
+if (projekteSection) {
+  // Linker Titel "Unsere"
+  const leftHeading = projekteSection.querySelector(projekteConfig.leftSelector);
+  if (leftHeading) {
+    createTitleAnimation(leftHeading, {
+      direction: "left",
+      trigger: "scrub",
+      triggerElement: projekteSection,
+      triggerStart: projekteConfig.leftTitle.triggerStart,
+      triggerEnd: projekteConfig.leftTitle.triggerEnd,
+      distance: "100vw",
+      scale: 2.5,
+      opacity: 0,
+      blur: projekteConfig.leftTitle.blur,
+      duration: projekteConfig.leftTitle.duration,
+      ease: "power2.out",
+    });
   }
+
+  // Rechter Titel "Projekte"
+  const rightHeading = projekteSection.querySelector(projekteConfig.rightSelector);
+  if (rightHeading) {
+    createTitleAnimation(rightHeading, {
+      direction: "right",
+      trigger: "scrub",
+      triggerElement: projekteSection,
+      triggerStart: projekteConfig.rightTitle.triggerStart,
+      triggerEnd: projekteConfig.rightTitle.triggerEnd,
+      distance: "100vw",
+      scale: 2.5,
+      opacity: 0,
+      blur: projekteConfig.rightTitle.blur,
+      duration: projekteConfig.rightTitle.duration,
+      ease: "power2.out",
+    });
+  }
+}
 });
