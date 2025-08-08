4d26611758aaec8b64fb41b7e674a3477889fb6a
EyecomDesign <71079977+EyecomDesign@users.noreply.github.com>
2025-08-08 14:40:10 +0200

    WIP Update

--- src/projects.js
+++ src/projects.js
@@ -81,9 +81,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
           ScrollTrigger.create({
             trigger: card,
-            start: "top bottom", // Triggers when card top reaches bottom of viewport (earlier)
+            start: "top 70%", // Triggers when card top reaches bottom of viewport (earlier)
             end: "top 70%",   // Animation completes when card top reaches center of viewport
-            onLeave: () => {
+            onEnter: () => {
               if (!hasAnimated) {
                 gsap.to(card, {
                   yPercent: 0,
