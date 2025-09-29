544b832e8a0d0d15b358cf8d2e60214853747ccf
EyecomDesign <71079977+EyecomDesign@users.noreply.github.com>
2025-09-29 15:12:18 +0200

    FIX

--- src/global.js
+++ src/global.js
@@ -1,4 +1,4 @@
-import "./typewriter.js";
+//import "./typewriter.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   // Typewriter animations
@@ -35,60 +35,60 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  // Navbar show/hide animation
-  const showAnim = gsap.fromTo(
-    ".navbar-fixed_component",
-    {
-      yPercent: -100,
-      opacity: 0,
-      display: "none",
-    },
-    {
-      yPercent: 0,
-      opacity: 1,
-      paused: true,
-      duration: 0.2,
-      display: "flex",
-    }
-  );
+  // // Navbar show/hide animation
+  // const showAnim = gsap.fromTo(
+  //   ".navbar-fixed_component",
+  //   {
+  //     yPercent: -100,
+  //     opacity: 0,
+  //     display: "none",
+  //   },
+  //   {
+  //     yPercent: 0,
+  //     opacity: 1,
+  //     paused: true,
+  //     duration: 0.2,
+  //     display: "flex",
+  //   }
+  // );
 
-  let scrollPositionWhenShown = 0;
+  // let scrollPositionWhenShown = 0;
 
-  const mainNav = document.querySelector(".padding-navbar");
-  let mainNavBottom = mainNav.offsetTop + mainNav.offsetHeight;
+  // const mainNav = document.querySelector(".padding-navbar");
+  // let mainNavBottom = mainNav.offsetTop + mainNav.offsetHeight;
 
-  // Recalculate when window resizes
-  window.addEventListener("resize", () => {
-    mainNavBottom = mainNav.offsetTop + mainNav.offsetHeight;
-  });
+  // // Recalculate when window resizes
+  // window.addEventListener("resize", () => {
+  //   mainNavBottom = mainNav.offsetTop + mainNav.offsetHeight;
+  // });
 
-  ScrollTrigger.create({
-    start: "top top",
-    end: "max",
-    onUpdate: (self) => {
-      const mainNavCompletelyGone = window.scrollY > mainNavBottom;
+  // ScrollTrigger.create({
+  //   start: "top top",
+  //   end: "max",
+  //   onUpdate: (self) => {
+  //     const mainNavCompletelyGone = window.scrollY > mainNavBottom;
 
-      if (!mainNavCompletelyGone && showAnim.progress() > 0) {
-        showAnim.reverse();
-      } else if (
-        self.direction === -1 &&
-        mainNavCompletelyGone &&
-        showAnim.progress() === 0
-      ) {
-        scrollPositionWhenShown = window.pageYOffset;
-        showAnim.play();
-      } else if (
-        self.direction === 1 &&
-        showAnim.progress() === 1 &&
-        mainNavCompletelyGone
-      ) {
-        const scrollOffset = window.pageYOffset - scrollPositionWhenShown;
-        const hideThreshold = 5;
+  //     if (!mainNavCompletelyGone && showAnim.progress() > 0) {
+  //       showAnim.reverse();
+  //     } else if (
+  //       self.direction === -1 &&
+  //       mainNavCompletelyGone &&
+  //       showAnim.progress() === 0
+  //     ) {
+  //       scrollPositionWhenShown = window.pageYOffset;
+  //       showAnim.play();
+  //     } else if (
+  //       self.direction === 1 &&
+  //       showAnim.progress() === 1 &&
+  //       mainNavCompletelyGone
+  //     ) {
+  //       const scrollOffset = window.pageYOffset - scrollPositionWhenShown;
+  //       const hideThreshold = 5;
 
-        if (scrollOffset > hideThreshold) {
-          showAnim.reverse();
-        }
-      }
-    },
-  });
+  //       if (scrollOffset > hideThreshold) {
+  //         showAnim.reverse();
+  //       }
+  //     }
+  //   },
+  // });
 });
